d3f70b93ef7230bec9fffd6bb52d1d6f3f32d17f
Szikszai László <sziklaszlo@gmail.com>
2015-10-10 12:56:37 +0200

    Model name fixes

--- catalog/controller/product/lastpieces.php
+++ catalog/controller/product/lastpieces.php
@@ -97,9 +97,9 @@ class ControllerProductLastpieces extends Controller {
 			'limit' => $limit
 		);
 
-		$product_total = $this->model_catalog_product->getTotalProductLastPieces($data);
+		$product_total = $this->model_catalog_productextended->getTotalProductLastPieces($data);
 
-		$results = $this->model_catalog_product->getProductLastPieces($data);
+		$results = $this->model_catalog_productextended->getProductLastPieces($data);
 
 		foreach ($results as $result) {
 			if ($result['image']) {
